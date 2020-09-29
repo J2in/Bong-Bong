bca978c8699ea316a8bbf93cb2eec6081a197b68
lazely <lz0136@naver.com>
2020-09-30 01:43:19 +0900

    자유롭게 임시파일 수정하시고 push 올리시면 됩니다 충돌나면.... 화이팅

---
+++ 임시파일.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Food{
+
+};
+
+class Drink : public Food{
+
+};
+class Pizza : public Food{
+
+};
+class Side : public Food{
+
+};
+
+class Table{
+	
+};
+class PaymentTotal{
+
+};
+
+
+int main(void){
+	
+	//ÆÄÀÌÆÃ 
+	return 0;
+}
+
